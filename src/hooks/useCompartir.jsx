9cbf1dad41ee3011c975ba0dd111009eeeaa49e3
COD3BREAK3R <hackermatrx1@gmail.com>
2024-07-22 22:55:11 -0400

    agregado eliminar intervalo al quitar una notificacion

--- src/hooks/useCompartir.jsx
+++ src/hooks/useCompartir.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import iconoCheck from '../assets/check-svgrepo-com.svg';
 import iconoError from '../assets/error-svgrepo-com.svg';
 
-
 export const useCompartir = () => {
     const [copiado, setCopiado] = useState(false);
     const [errorCopia, setErrorCopia] = useState(false);
+    const [timeoutId, setTimeoutId] = useState(null);
 
     const urlActual = location.href;
 
@@ -14,27 +14,42 @@ export const useCompartir = () => {
             await navigator.clipboard.writeText(urlActual);
             setCopiado(true);
 
-            setTimeout(() => {
-                setCopiado(false)
+            const id = setTimeout(() => {
+                setCopiado(false);
             }, 3000);
+            setTimeoutId(id);
 
         } catch (error) {
             console.error('Error al copiar al portapapeles:', error);
             setErrorCopia(true);
 
-            setTimeout(() => {
-                setErrorCopia(false)
+            const id = setTimeout(() => {
+                setErrorCopia(false);
             }, 3000);
+            setTimeoutId(id);
         }
     };
 
     const renderNotificacion = () => {
         if (copiado) {
-            return <div className='notificacion' onClick={() => setCopiado(false)}>Enlace copiado al portapapeles <img src={iconoCheck} /></div>;
-
+            return (
+                <div className='notificacion' onClick={() => {
+                    setCopiado(false);
+                    clearTimeout(timeoutId);
+                }}>
+                    Enlace copiado al portapapeles <img src={iconoCheck} alt="Check icon" />
+                </div>
+            );
         }
         if (errorCopia) {
-            return <div className='notificacion error' onClick={() => setErrorCopia(false)}>No se pudo copiar el enlace <img src={iconoError} /> </div>
+            return (
+                <div className='notificacion error' onClick={() => {
+                    setErrorCopia(false);
+                    clearTimeout(timeoutId);
+                }}>
+                    No se pudo copiar el enlace <img src={iconoError} alt="Error icon" />
+                </div>
+            );
         }
     };
 
